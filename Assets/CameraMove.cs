9b63e39799be63a04c65e198e709dc7501fe2a12
smallru8 <30855604+smallru8@users.noreply.github.com>
2021-02-14 10:07:28 +0800

    First upload

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public float moveSpeed = 50f;
+    public float rotateSpeed = 0.1f;
+    public float scrollSpeed = 1.5f;
+    void Update()
+    {
+        //平移
+        if (Input.GetMouseButton(1))
+        {
+            float h = Input.GetAxis("Mouse X") * moveSpeed * Time.deltaTime;
+            float v = Input.GetAxis("Mouse Y") * moveSpeed * Time.deltaTime;
+            this.transform.Translate(-h, -v, 0);
+        }
+        //旋轉
+        if (Input.GetMouseButton(0)) 
+        {
+            var x = Input.GetAxis("Mouse X") * rotateSpeed;
+            var y = Input.GetAxis("Mouse Y") * rotateSpeed;
+            this.transform.Rotate(0,x,0, Space.World);
+            this.transform.Rotate(-y, 0, 0);
+        }
+        //縮放
+        var z = 0f;
+        if ((z=Input.GetAxis("Mouse ScrollWheel")) > 0f|| (z=Input.GetAxis("Mouse ScrollWheel")) < 0f)
+        {
+            this.transform.Translate(0, 0, z);
+        }
+    }
+}
