6f9f9c0d6e3647cd3710652a9d267f117cb6e372
smallru8 <30855604+smallru8@users.noreply.github.com>
2021-02-15 11:28:01 +0800

    add XYZ axis

--- Assets/Outline.cs
+++ Assets/Outline.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class Outline : MonoBehaviour
 {
     public static GameObject preObj = null;
+
+    [SerializeField]
+    GameObject x_axis = null;
+    [SerializeField]
+    GameObject y_axis = null;
+    [SerializeField]
+    GameObject z_axis = null;
+
+    public static GameObject instanceX;
+    public static GameObject instanceY;
+    public static GameObject instanceZ;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +37,9 @@ public class Outline : MonoBehaviour
                 int len = preObj.GetComponent<MeshRenderer>().materials.Length;
                 Material[] materials = { preObj.GetComponent<MeshRenderer>().materials[len - 1] };
                 preObj.GetComponent<MeshRenderer>().materials = materials;
+                Destroy(instanceX);
+                Destroy(instanceY);
+                Destroy(instanceZ);
             }
 
             preObj = gameObject;//紀錄現在編輯的Obj
@@ -36,6 +50,16 @@ public class Outline : MonoBehaviour
                 Material outline = new Material(Shader.Find("Outlined/UltimateOutline"));
                 Material[] newMaterials = { outline, myRend.materials[0] };
                 myRend.materials = newMaterials;
+
+                instanceX = GameObject.Instantiate<GameObject>(x_axis, null);
+                instanceX.transform.SetParent(transform);
+                instanceX.transform.position = transform.position;
+                instanceY = GameObject.Instantiate<GameObject>(y_axis, null);
+                instanceY.transform.SetParent(transform);
+                instanceY.transform.position = transform.position;
+                instanceZ = GameObject.Instantiate<GameObject>(z_axis, null);
+                instanceZ.transform.SetParent(transform);
+                instanceZ.transform.position = transform.position;
             }
         }
     }
